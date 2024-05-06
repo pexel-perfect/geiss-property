2f4dd908e20a3e4a069405d9f3d9a21820a5a2bc
riyazul <riyazulhaque64@gmail.com>
2024-05-06 10:33:45 +0600

    header modified

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -52,21 +52,27 @@ export const Navbar = () => {
 	);
 
 	return (
-		<NextUINavbar maxWidth="xl" position="sticky">
+		<NextUINavbar maxWidth="xl" position="sticky" className="py-4">
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
-						<p className="font-bold text-inherit">ACME</p>
 					</NextLink>
 				</NavbarBrand>
+
+			</NavbarContent>
+
+			<NavbarContent
+				className="hidden sm:flex basis-1/5 sm:basis-full"
+				justify="end"
+			>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
 						<NavbarItem key={item.href}>
 							<NextLink
 								className={clsx(
 									linkStyles({ color: "foreground" }),
-									"data-[active=true]:text-primary data-[active=true]:font-medium"
+									"data-[active=true]:text-primary data-[active=true]:font-medium uppercase font-medium"
 								)}
 								color="foreground"
 								href={item.href}
@@ -76,28 +82,21 @@ export const Navbar = () => {
 						</NavbarItem>
 					))}
 				</ul>
-			</NavbarContent>
-
-			<NavbarContent
-				className="hidden sm:flex basis-1/5 sm:basis-full"
-				justify="end"
-			>
 				<NavbarItem className="hidden sm:flex gap-2">
-					
 					<ThemeSwitch />
 				</NavbarItem>
-				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-				
-			</NavbarContent>
+				{/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
 
-			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">				
+			</NavbarContent>
+			{/* Mobile menu */}
+			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
 				<ThemeSwitch />
 				<NavbarMenuToggle />
 			</NavbarContent>
 
 			<NavbarMenu>
-				{searchInput}
-				<div className="mx-4 mt-2 flex flex-col gap-2">
+				{/* {searchInput} */}
+				<div className="mx-4 mt-6 flex flex-col gap-2">
 					{siteConfig.navItems.map((item, index) => (
 						<NavbarMenuItem key={`${item}-${index}`}>
 							<Link
@@ -105,8 +104,8 @@ export const Navbar = () => {
 									index === 2
 										? "primary"
 										: index === siteConfig.navItems.length - 1
-										? "danger"
-										: "foreground"
+											? "danger"
+											: "foreground"
 								}
 								href="#"
 								size="lg"
