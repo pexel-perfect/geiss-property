49f25c87cb1a99c191166d6943f385413f142616
riyazul <riyazulhaque64@gmail.com>
2024-05-06 09:23:37 +0600

    Boiler template with Next js, next ui, tailwind, typescript, header, gooter

---
+++ config/site.ts
@@ -0,0 +1,33 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "Next.js + NextUI",
+	description: "Make beautiful websites regardless of your design experience.",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Über uns",
+			href: "/uber-uns",
+		},
+		{
+			label: "Angebote",
+			href: "/angebote",
+		},
+		{
+			label: "Dubai-News",
+			href: "/dubai-news",
+		},
+		{
+			label: "Services",
+			href: "/services",
+		},
+		{
+			label: "Kontaktformular",
+			href: "/kontaktformular",
+		}
+	],
+	
+};
