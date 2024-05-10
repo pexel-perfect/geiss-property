bb82b2be58c4e1bcea16461dafe7733e74b4f3ec
riyazul <riyazulhaque64@gmail.com>
2024-05-11 01:45:16 +0600

    single page and mobile navbar icon is fiexed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,38 @@
-import { Link } from "@nextui-org/link";
+'use client'
+import React, { useState } from 'react';
+import { Logo } from "@/components/icons";
+import { siteConfig } from "@/config/site";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
+// Replace "your-close-icon-library" with the actual library you're using
 import {
 	NavbarBrand,
 	NavbarContent,
 	NavbarItem,
-	NavbarMenu,
-	NavbarMenuItem,
-	NavbarMenuToggle,
-	Navbar as NextUINavbar,
+	Navbar as NextUINavbar
 } from "@nextui-org/navbar";
-import { siteConfig } from "@/config/site";
 import clsx from "clsx";
 import NextLink from "next/link";
-import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	const handleMobileMenuToggle = () => {
+		setIsMobileMenuOpen((prev) => !prev);
+	};
+
+	const closeMobileMenu = () => {
+		setIsMobileMenuOpen(false);
+	};
+
 	return (
-		<NextUINavbar maxWidth="xl" position="sticky" className="py-4">
+		<NextUINavbar maxWidth="xl" position="sticky" className="py-4 relative">
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
 					</NextLink>
 				</NavbarBrand>
-
 			</NavbarContent>
 
 			<NavbarContent
@@ -34,7 +44,7 @@ export const Navbar = () => {
 						<NavbarItem key={item.href}>
 							<NextLink
 								className={clsx(
-									"data-[active=true]:text-light text-light data-[active=true]:font-medium uppercase  font-secondary font-[500] text-[17px]"
+									"data-[active=true]:text-light text-light data-[active=true]:font-medium uppercase font-secondary font-[500] text-[17px]"
 								)}
 								href={item.href}
 							>
@@ -43,29 +53,35 @@ export const Navbar = () => {
 						</NavbarItem>
 					))}
 				</ul>
-
-
 			</NavbarContent>
+
 			{/* Mobile menu */}
-			<NavbarContent className="sm:hidden basis-1 pl-4 text-primary" justify="end">
-				<NavbarMenuToggle />
-			</NavbarContent>
+			<div
+				className="sm:hidden basis-1 pl-4 text-primary cursor-pointer"
+				onClick={handleMobileMenuToggle}
+			>
+				{isMobileMenuOpen ?<AiOutlineClose /> : <RxHamburgerMenu />}
+			</div>
 
-			<NavbarMenu >
-				<div className="mx-4 mt-6 flex flex-col gap-2">
-					{siteConfig.navItems.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`} >
-							<Link
-								className="text-light"
-								href={item.href}
-								size="lg"
-							>
-								{item.label}
-							</Link>
-						</NavbarMenuItem>
-					))}
+			{isMobileMenuOpen && (
+				<div className="fixed top-44 right-0 w-full h-full z-50 flex justify-center items-center ">
+					<div className="bg-grayBackground w-3/4 max-w-sm rounded shadow-lg p-4">
+
+						<div className="mt-4">
+							{siteConfig.navItems.map((item, index) => (
+								<NextLink
+									key={index}
+									className="block py-2 text-description hover:text-grayBackground hover:bg-gray-200 px-4"
+									href={item.href}
+									onClick={closeMobileMenu}
+								>
+									{item.label}
+								</NextLink>
+							))}
+						</div>
+					</div>
 				</div>
-			</NavbarMenu>
+			)}
 		</NextUINavbar>
 	);
 };
